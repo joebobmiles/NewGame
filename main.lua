4ed67abf4ebcfe5341c153458f753f27d37409b4
Joseph R Miles <joe@jrm.dev>
2022-04-26 18:34:25 -0700

    feat: grid follows mouse

--- main.lua
+++ main.lua
@@ -1,3 +1,55 @@
+GRID_CELL_DIMENSION = 16
+
+function love.load()
+    gridTransform = { 0, 0 }
+
+    gridCanvas = love.graphics.newCanvas()
+
+    gridCanvas:renderTo(function ()
+        local width, height = love.graphics.getDimensions()
+
+        love.graphics.setColor(255, 255, 255, 1)
+        love.graphics.setLineWidth(1)
+
+        for x = gridTransform[1] % 16, width, GRID_CELL_DIMENSION
+        do
+            love.graphics.line(x, 0, x, height)
+        end
+
+        for y = gridTransform[2] % 16, height, GRID_CELL_DIMENSION
+        do
+            love.graphics.line(0, y, width, y)
+        end
+    end)
+end
+
+function love.update(t)
+    local x, y = love.mouse.getPosition()
+
+    if x ~= gridTransform[1] or y ~= gridTransform[2] then
+        gridTransform = { x, y }
+
+        gridCanvas:renderTo(function ()
+            love.graphics.clear()
+
+            local width, height = love.graphics.getDimensions()
+
+            love.graphics.setColor(255, 255, 255, 1)
+            love.graphics.setLineWidth(1)
+
+            for x = gridTransform[1] % 16, width, GRID_CELL_DIMENSION
+            do
+                love.graphics.line(x, 0, x, height)
+            end
+
+            for y = gridTransform[2] % 16, height, GRID_CELL_DIMENSION
+            do
+                love.graphics.line(0, y, width, y)
+            end
+        end)
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.draw(gridCanvas)
 end
